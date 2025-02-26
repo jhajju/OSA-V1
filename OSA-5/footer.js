d6364ca1783ba764254e37aa51c5c295220d17da
jhajju <jhayxera1419@gmail.com>
2025-02-26 11:38:19 +0800

    Delete OSA-5 directory

--- OSA-5/footer.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("footer.html") // Load the footer from footer.html
-        .then(response => response.text())
-        .then(data => {
-            document.body.insertAdjacentHTML("beforeend", data);
-        })
-        .catch(error => console.error("Error loading footer:", error));
-});
